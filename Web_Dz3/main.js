737df07d9b29a3a1a5b63000d15b2c6254443e9f
Chiefpro <chiefpro@yandex.ru>
2023-08-12 02:11:28 +0300

    web_dz3.0

---
+++ Web_Dz3/main.js
@@ -0,0 +1,28 @@
+// let a = Number.parseInt(prompt("Введи число А "));
+// let b = Number.parseInt(prompt("Введи число B "));
+// alert(`Результат сложения ${a} и ${b} = ${a + b}`);
+
+// function sum(n1, n2) {
+//     const n3 = n1 + n2;
+//     return n3;
+// }
+
+// alert(`Результат сложения через функцию ${a} и ${b} = ${sum(a, b)}`);
+
+// const life = confirm("Хорошо живется? ");
+// console.log(life);
+// if (life){
+//     alert("Тогда мы имем к вам!!")
+// } else {
+//     alert("Ну, догда держись там!!")
+// }
+
+// const life2 = (confirm("Хорошо живется? ")) ? alert("OKKKKKKK") : alert("NOOOOOOO");
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+greeting(prompt("Введите пожалуйста Ваше имя => "));
+
+function greeting(n) {
+    console.log(`Привет ${n}`);
+}
