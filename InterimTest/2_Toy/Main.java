1c0673708702e5420f06db29d6252c593e2bd55f
Chiefpro <chiefpro@yandex.ru>
2024-05-04 18:48:11 +0300

    IT 2.1

---
+++ InterimTest/2_Toy/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        ToyStore toyStore = new ToyStore();
+    
+        Toy toy1 = new Toy(1, "Чебурашка", 5, 20);
+        toyStore.addToy(toy1);
+    
+        Toy toy2 = new Toy(2, "Кукла Маша", 10, 30);
+        toyStore.addToy(toy2);
+    
+        Toy toy3 = new Toy(3, "Робот", 15, 40);
+        toyStore.addToy(toy3);
+    
+        toyStore.showAvailableToys();
+    
+        toyStore.updateToyWeight(3, 90);
+    
+        toyStore.playToys();
+    
+        toyStore.showAvailableToys();
+    }
+    }
